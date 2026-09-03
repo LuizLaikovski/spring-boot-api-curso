7c6022eea2b654bfccf69f84fceead14f41af5eb
Luiz Henrique Pasin Laikovski <160736729+LuizLaikovski@users.noreply.github.com>
2026-09-03 12:44:10 -0300

    inciando o projeto

---
+++ src/main/java/com/spring_boot_api/luiz_laikovski/LuizLaikovskiApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_api.luiz_laikovski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuizLaikovskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuizLaikovskiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot_api/luiz_laikovski/LuizLaikovskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_api.luiz_laikovski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuizLaikovskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
